e91d361d96a34cfd1f0b2a03de74fde87bc0170a
DESKTOP-1LI8HSU\Omar <omarfawzi96@gmail.com>
2017-05-13 16:25:13 +0200

    Notification Service added

---
+++ src/main/java/learnforfun/mvc/Services/NotificationService.java
@@ -0,0 +1,21 @@
+package learnforfun.mvc.Services;
+
+import learnforfun.mvc.DAO.NotificationDAO;
+import learnforfun.mvc.DAOImp.NotificationDAOImpl;
+import learnforfun.mvc.Models.Notifications;
+
+import javax.management.Notification;
+import java.util.ArrayList;
+
+/**
+ * Created by Omar on 13-May-17.
+ */
+public class NotificationService {
+    private NotificationDAO notificationDAO = new NotificationDAOImpl();
+    public void insert(Notifications notification) {
+        notificationDAO.insert(notification);
+    }
+    public ArrayList<Notifications> getNotifications(String notifiedUser) {
+        notificationDAO.getNotifications(notifiedUser);
+    }
+}
